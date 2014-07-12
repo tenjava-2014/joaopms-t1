ebbec4c0af9b2c811e9f791a2bbb9b7afee674d6
João Pedro <joni_silvaco@hotmail.com>
2014-07-12 02:47:58 +0100

    Checking if the item is a stick and if was a specific name

--- src/main/java/com/tenjava/entries/joaopms/t1/event/UpgradeCraftEvent.java
+++ src/main/java/com/tenjava/entries/joaopms/t1/event/UpgradeCraftEvent.java
@@ -1,9 +1,12 @@
 package com.tenjava.entries.joaopms.t1.event;
 
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class UpgradeCraftEvent implements Listener {
     @EventHandler
@@ -14,10 +17,27 @@ public class UpgradeCraftEvent implements Listener {
         if (action != Action.LEFT_CLICK_BLOCK)
             return;
 
-        // Check if the item is null
-        if (event.getItem() == null)
+        // Checks if the item is null
+        ItemStack item = event.getItem();
+        if (item == null)
             return;
 
+        // Checks if the item is a stick
+        if (item.getType() != Material.STICK)
+            return;
+
+        ItemMeta itemMeta = item.getItemMeta();
+        String itemName = itemMeta.getDisplayName();
+
+        // Checks if the stick's name is null
+        if (itemName == null)
+            return;
+
+        // Checks if the stick's name is "Upgrade Wand"
+        if (!itemName.equalsIgnoreCase("Upgrade Wand"))
+            return;
+        
+        event.setCancelled(true);
         System.out.println("random thing goes here");
     }
 }
