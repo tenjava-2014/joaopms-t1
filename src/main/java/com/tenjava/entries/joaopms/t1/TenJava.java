3fe7cb0e1e227d51e6e7fdc15294ae96994499ab
João Pedro <joni_silvaco@hotmail.com>
2014-07-12 01:14:31 +0100

    Added a way to get the plugin instance

--- src/main/java/com/tenjava/entries/joaopms/t1/TenJava.java
+++ src/main/java/com/tenjava/entries/joaopms/t1/TenJava.java
@@ -1,5 +1,23 @@
 package com.tenjava.entries.joaopms.t1;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+    private static Plugin plugin;
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+        System.out.println("Hello world!");
+    }
+
+    /**
+     * Gets the plugin instance
+     *
+     * @return the plugin instance
+     */
+    public static Plugin getPlugin() {
+        return plugin;
+    }
+}
