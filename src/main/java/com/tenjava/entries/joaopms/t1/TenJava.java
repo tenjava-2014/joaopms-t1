7c563131b050df9cd4a51bab25d4b79f50fb7daf
João Pedro <joni_silvaco@hotmail.com>
2014-07-12 01:23:28 +0100

    Added a way to get the plugin instance and log messages easier

--- src/main/java/com/tenjava/entries/joaopms/t1/TenJava.java
+++ src/main/java/com/tenjava/entries/joaopms/t1/TenJava.java
@@ -3,21 +3,33 @@ package com.tenjava.entries.joaopms.t1;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public class TenJava extends JavaPlugin {
     private static Plugin plugin;
 
     @Override
     public void onEnable() {
         plugin = this;
-        System.out.println("Hello world!");
+        log(Level.INFO, "Hello world!");
     }
 
     /**
      * Gets the plugin instance
      *
-     * @return the plugin instance
+     * @return The plugin instance
      */
     public static Plugin getPlugin() {
         return plugin;
+    }
+
+    /**
+     * Logs a message to the console
+     *
+     * @param level   The message level
+     * @param message The message
+     */
+    public static void log(Level level, Object message) {
+        plugin.getLogger().log(level, message.toString());
     }
 }
