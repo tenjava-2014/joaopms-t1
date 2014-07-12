185c34cb9c0df9766f2da54ac942fa2058c795f6
João Pedro <joni_silvaco@hotmail.com>
2014-07-12 02:29:45 +0100

    Checking if the vehicle is a boat or a minecart

--- src/main/java/com/tenjava/entries/joaopms/t1/event/VehicleUpgradeClick.java
+++ src/main/java/com/tenjava/entries/joaopms/t1/event/VehicleUpgradeClick.java
@@ -1,5 +1,6 @@
 package com.tenjava.entries.joaopms.t1.event;
 
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,6 +11,11 @@ public class VehicleUpgradeClick implements Listener {
     public void vehicleDamageEvent(VehicleDamageEvent event) {
         // Check if the attacker is a player
         if (!(event.getAttacker() instanceof Player))
+            return;
+
+        EntityType entityType = event.getVehicle().getType();
+        // Check if the vehicle is a Minecart or a Boat
+        if (!(entityType == EntityType.MINECART || entityType == EntityType.BOAT))
             return;
 
         event.setCancelled(true);
